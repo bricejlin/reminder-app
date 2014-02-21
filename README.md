1e6a830782f7e7e13387c4adf5c33fa5c06c3220
bricejlin <bricejlin@gmail.com>
2014-02-20 19:23:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+A reminder app that alerts a user on a customizable interval to look away from the screen.
+
+To do:
+- Short Term
+  - Change button to confirmation text
+
+- Long Term
+  - Can change interval without reloading page
---
+++ js/views/reminder-view.js
@@ -0,0 +1,27 @@
+ReminderView = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    // Compile the template using underscore
+    var template = _.template( $('#reminder_template').html() );
+    // Load the compiled HTML into the Backbone 'el'
+    this.$el.html(template);
+  },
+  events: {
+    'click #remind-btn': 'setReminder'
+  },
+  setReminder: function(event) {
+    // Button clicked, you can access the element that was clicked
+    // event.currentTarget
+    var interval = 60000 * ( Number( $('#reminder-input').val() ) || 20 );
+
+    // Set alert interval
+    setInterval(function() {
+      alert(interval/60000 + ' minutes up! Time for 20 second break!!');
+    }, interval);
+
+    // Log confirmation for interval in minutes
+    console.log('Great! Will remind you every ' + interval/60000 + ' minutes!');
+  }
+});
